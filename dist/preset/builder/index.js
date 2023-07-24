b402caa1eb2e39eef96e3229dec3ab59007b9cb8
junjie.chen <xiyueedu@gmail.com>
2023-07-24 18:04:21 +0800

    add test

--- dist/preset/builder/index.js
+++ dist/preset/builder/index.js
@@ -16,3 +16,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./kernel"), exports);
 __exportStar(require("./simpleAccount"), exports);
+__exportStar(require("./testAccount"), exports);
